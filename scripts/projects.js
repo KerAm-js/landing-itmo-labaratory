2abe2660a3615bf4fd2a4a057bc96f78f2ec56aa
Amir Kerimov <95amirk@gmail.com>
2022-04-06 13:16:06 +0300

    feat: Add content filtration

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,16 +1,27 @@
-
 const navSelectedItem = document.querySelector('.projects__nav-selected-item');
 const navSelectedItemText = document.querySelector('.projects__nav-selected-item-text');
 const arrowImg = document.querySelector('.projects__arrow-img');
 const navList = document.querySelector('.projects__nav-list');
 const navItems = Array.from(document.querySelectorAll('.projects__nav-item'));
+const navActiveItem = navList.querySelector('.projects__nav-item_active');
 const cardList = document.querySelector('.projects__card-list');
 const cardTemplate = document.querySelector('#projects__card').content;
 
-function renderCards() {
+function clearCardList() {
   cardList.innerHTML = '';
-  
-  projectsData.forEach(item => {
+}
+
+function renderCards(filter = 'Все проекты') {
+  clearCardList();
+  const filteredData = projectsData.filter(item => {
+    if (filter === 'Все проекты') {
+      return true;
+    } else {
+      return item.categories.includes(filter);
+    }
+  })
+
+  filteredData.forEach(item => {
     const cardElement = cardTemplate.querySelector('.projects__card').cloneNode(true);
     cardElement.querySelector('.projects__card-description').textContent = item.description;
     cardElement.querySelector('.projects__card-link').href = item.link;
@@ -42,22 +53,24 @@ function onWindowResize() {
   })
 }
 
-function toggleContent(evt) {
-  document.querySelector('.projects__nav-item_active').classList.remove('projects__nav-item_active');
-  evt.target.classList.add('projects__nav-item_active');
-  if (window.screen.width < 580) {
-    navSelectedItemText.textContent = evt.target.textContent;
-    toggleSelectPopupOpened();
-  }
-}
-
 function toggleSelectPopupOpened() {
   navList.classList.toggle('projects__nav-list_opened');
   navSelectedItem.classList.toggle('projects__nav-selected-item_opened');
   arrowImg.classList.toggle('projects__arrow-img_rotated');
 }
 
+function toggleContent(evt) {
+  const filter = evt.target.textContent;
+  document.querySelector('.projects__nav-item_active').classList.remove('projects__nav-item_active');
+  evt.target.classList.add('projects__nav-item_active');
+  renderCards(filter.trim());
+  if (window.screen.width < 580) {
+    navSelectedItemText.textContent = filter;
+    toggleSelectPopupOpened();
+  }
+}
+
 navItems.forEach(item => item.addEventListener('click', toggleContent));
 navSelectedItem.addEventListener('click', toggleSelectPopupOpened);
 window.addEventListener('resize', onWindowResize);
-renderCards();
+renderCards(navActiveItem.textContent.trim());
